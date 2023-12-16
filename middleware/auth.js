7a563e29d304243ede67647deab60d8897a77f76
NoNameGivenCoder <chaytontimedis@gmail.com>
2023-12-16 14:43:59 -0700

    improved auth and started work on account generation

--- middleware/auth.js
+++ middleware/auth.js
@@ -10,20 +10,21 @@ async function auth(req, res, next) {
 
     //Assigning variables
     var param_pack = req.get('x-nintendo-parampack');
-    var service_token = req.get('x-nintendo-servicetoken').slice(0, 42);
+    var service_token = req.get('x-nintendo-servicetoken')
 
     //Check if the request is faulty or not.
-    if (!service_token || !param_pack) { res.sendStatus(401); console.log("[ERROR] (%s) Recieved either no Param Pack or no Service Token.".red, moment().format("HH:mm:ss")); return;}
+    if (!service_token || !param_pack || service_token.length < 42) { res.sendStatus(401); console.log("[ERROR] (%s) Recieved either no Param Pack, no Service Token, or invalid Service Token.".red, moment().format("HH:mm:ss")); return;}
+
+    service_token = service_token.slice(0, 42);
 
     //Translating Param_Pack into a more readable format to collect data from.
     var base64Result = (Buffer.from(param_pack, 'base64').toString()).slice(1, -1).split("\\");
-    req.param_pack = {}
+    req.param_pack = {};
+    req.service_token = service_token;
 
     for (let i = 0; i < base64Result.length; i += 2) {
         req.param_pack[base64Result[i].trim()] = base64Result[i + 1].trim();
     }
-
-    //TODO: add proper account auth when we make the users table in the database
 
     //Grabbing the correct service token
     var sql;
@@ -39,17 +40,14 @@ async function auth(req, res, next) {
             break;
     }
 
-    console.log(sql);
-
+    //Grabbing account from database
     var account = await query(sql, service_token);
-    
-    //If there is no account, then send a 401 (Unauthorized)
-    if (account.length == 0) {res.sendStatus(401); return;}
+
+    //If there is no account AND the request isn't creating an account, then send a 401 (Unauthorized)
+    if ((account.length == 0) && !(req.path.includes('people'))) {res.sendStatus(401); return;}
 
     //Finally, set the requests account to be the newly found account from the database
-    req.account = account
-
-    console.log(account)
+    req.account = account;
 
     next();
 }
--- routes/v1/people.js
+++ routes/v1/people.js
@@ -8,11 +8,18 @@ const multer = require('multer')
 const con = require('../../database_con');
 const query = util.promisify(con.query).bind(con);
 
+const axios = require('axios');
+
+const sha256 = require('sha256');
+
 route.post("/", multer().none(), async (req, res) => {
     //Checking to make sure request doesn't already have an account attached
-    if (req.account) {res.sendStatus(403); return;}
+    if (req.account.length >= 1) {res.sendStatus(403); console.log("[ERROR] (%s) Account is already created.".red, moment().format("HH:mm:ss")); return;}
     
+    //Grabbing neccesary login details
     var nnid = req.body.nnid;
+    var service_token = req.service_token;
+    var password = req.body.password;
 
 })
 
