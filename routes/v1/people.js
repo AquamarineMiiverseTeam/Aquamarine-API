2358a9b5e15732ec26474086555e03570c6672d5
NoNameGivenCoder <chaytontimedis@gmail.com>
2024-01-14 16:41:20 -0700

    changed response to be 200 in v1/people

--- routes/v1/people.js
+++ routes/v1/people.js
@@ -45,11 +45,7 @@ route.post("/", multer().none(), async (req, res) => {
     await query(`INSERT INTO accounts (pid, nnid, mii, mii_name, mii_hash, bio, admin, banned, ${req.platform}_service_token, password_hash, password_salt, language, country, game_experience) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?)`, 
     [account_json.pid, nnid, account_json.data, account_json.name, account_json.images.hash, "User has not set a bio yet..", 0, 0, service_token, passwordHash, salt, language, country, game_experience]);
     
-    if (req.platform == "3ds") {
-        res.redirect(`https://${endpoint_config.n3ds_url}/account/account_created`);
-    } else {
-        res.redirect(`https://${endpoint_config.portal_url}/account/account_created`);
-    }
+    res.sendStatus(200)
 
     console.log("[INFO] (%s) Created account for %s.".blue, moment().format("HH:mm:ss"), nnid)
 })
