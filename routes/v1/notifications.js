652fce32a9cd2f5bdb83a48dc27eca10e9f0aa33
NoNameGivenCoder <chaytontimedis@gmail.com>
2024-01-21 10:42:33 -0700

    sending length instead of notifications in v1/notifications

--- routes/v1/notifications.js
+++ routes/v1/notifications.js
@@ -10,10 +10,11 @@ const con = require('../../../Aquamarine-Utils/database_con');
 const query = util.promisify(con.query).bind(con);
 
 route.get("/", async (req, res) => {
-    const notifications = await query("SELECT * FROM notifications WHERE account_id=? ORDER BY create_time DESC", req.account[0].id)
+    const notifications_messages = await query("SELECT * FROM notifications WHERE account_id=? AND read_notif=0 AND type='message' GROUP BY yeah_post_id ORDER BY create_time DESC", req.account[0].id)
+    const notifications_normal = await query("SELECT * FROM notifications WHERE account_id=? AND read_notif=0 AND NOT type='message' GROUP BY yeah_post_id, ban_id", req.account[0].id)
 
     if (req.get("content-type") == "text/json") {
-        res.status(200).send(notifications);
+        res.status(200).send({message_count : notifications_messages.length, notification_count : notifications_normal.length});
     } else {
         //Here's where XML specific code would go, we don't have it fully ready though.
     }
