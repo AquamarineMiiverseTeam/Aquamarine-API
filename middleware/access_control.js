46c33d5a14be25e07be3e7fbb23cfe7ca41c3a66
NoNameGivenCoder <chaytontimedis@gmail.com>
2024-02-07 20:51:15 -0700

    fixed access_control

--- middleware/access_control.js
+++ middleware/access_control.js
@@ -1,5 +1,5 @@
 function access_control(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', "https://*.nonamegiven.xyz");
+    res.setHeader('Access-Control-Allow-Origin', req.get("origin"));
 
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'x-nintendo-servicetoken,x-nintendo-parampack,content-type');
