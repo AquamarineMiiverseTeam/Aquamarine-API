e5deb0e0386bd00d1840b087f0808f5a11bee0fa
NoNameGivenCoder <chaytontimedis@gmail.com>
2024-04-06 19:13:03 -0700

    added env variable for port

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 const express = require('express');
-const path = require('path');
-const util = require('util')
 const colors = require('colors');
 
 const app = express();
 
-const config_http = require('./config/http.json');
 const logger = require('./middleware/log');
-const db_con = require('../Aquamarine-Utils/database_con');
+const db_con = require('../shared_config/database_con');
 
 //Grab logger and auth middleware and use it. (Logs all incoming HTTP/HTTPS requests)
-const auth = require('../Aquamarine-Utils/middleware/auth_middleware');
+const auth = require('../shared_config/middleware/auth_middleware');
 const access_control = require("./middleware/access_control")
 const api_logger = require("./middleware/api_logger")
 
@@ -34,17 +31,17 @@ for (const route of routes.v2) {
 
 //Set our app to listen on the config port
 app.listen(config_http.port, async () => {
-    console.log("[INFO] Listening on port %d".green, config_http.port);
+    console.log("[INFO] Current Environment: %s. Listening on port %d".green, JSON.parse(process.env.ENVIRONMENT)['ENV_NAME'], process.env.PORT);
 
     //Initializing database
-    await db_con.raw("SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));")
+    await db_con.env_db.raw("SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));")
     
     //TODO: add in account table and show number of accounts generated
-    var num_posts = (await db_con("posts").count())[0]['count(*)']
-    var num_communities = (await db_con("communities").count())[0]['count(*)']
-    var num_accounts = (await db_con("accounts").count())[0]['count(*)']
-    var num_empathies = (await db_con("empathies").count())[0]['count(*)']
-    var num_favorites = (await db_con("favorites").count())[0]['count(*)']
+    var num_posts = (await db_con.env_db("posts").count())[0]['count(*)']
+    var num_communities = (await db_con.env_db("communities").count())[0]['count(*)']
+    var num_accounts = (await db_con.account_db("accounts").count())[0]['count(*)']
+    var num_empathies = (await db_con.env_db("empathies").count())[0]['count(*)']
+    var num_favorites = (await db_con.env_db("favorites").count())[0]['count(*)']
 
     logger.log(`Number of Posts: ${num_posts}`)
     logger.log(`Number of Communities: ${num_communities}`)
